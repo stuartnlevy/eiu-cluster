ed282990d306bb319b4437cfb1cc94f1397e4ee8
Stuart Levy <stuartnlevy@gmail.com>
2025-06-11 21:49:00 -0500

    Add -bgeobase.  Report etot (total energy) for each star when writing data table.  Write a full set of .sdbs and corresponding .speck if -sdbdump.

--- survey-snap.py
+++ survey-snap.py
@@ -13,11 +13,13 @@ stepmax = 9999999
 rscale = 1
 orbs = []
 sdbout = None
+outbase = 0
 outstem = None
+sdbstem = None
 heartid = None
 
 def Usage():
-    print(f"""Usage: {sys.argv[0]}  [-e everyNth] [-orbs id0,id1,...] [-trackbh] [-o out.bgeo] nemofile""")
+    print(f"""Usage: {sys.argv[0]}  [-e everyNth] [-orbs id0,id1,...] [-trackbh] [-o out.bgeo] [-bgeoobase outbasenum] nemofile""")
     sys.exit(1)
 
 ii = 1
@@ -32,8 +34,10 @@ while ii<len(sys.argv) and sys.argv[ii][0] == '-' and len(sys.argv[ii]) > 1:
         orbs.extend( [ int(s) for s in ss ] ) 
     elif opt == '-o':
         outstem = sys.argv[ii]; ii += 1
+    elif opt == '-bgeobase':
+        outbase = int( sys.argv[ii] ); ii += 1
     elif opt == '-sdbdump':
-        sdbout = sys.argv[ii]; ii += 1
+        sdbstem = sys.argv[ii]; ii += 1
     elif opt == '-stepmax':
         stepmax = int( sys.argv[ii] ); ii += 1
     elif opt == '-trackbh':
@@ -49,7 +53,7 @@ if len(sys.argv) != ii+1:
 
 nemoin = sys.argv[ii]
 
-cmd = f"snapprint in='{nemoin}' options=x,y,z,i,m header=t"
+cmd = f"snapprint in='{nemoin}' options=x,y,z,i,m,etot header=t"
 
 cmdf = os.popen(cmd, 'r')
 
@@ -68,7 +72,7 @@ while stepno <= stepmax:
         raise ValueError("Expected <nbodies> <timeval> -- what's " + line)
     nbodies, time = int(ss[0]), float(ss[1])
     #print(f"# {nbodies=} {time=}", end="", flush=True)
-    pts = numpy.empty( (nbodies, 3+2) )
+    pts = numpy.empty( (nbodies, 3+3) )
     for i in range(nbodies):
         line = cmdf.readline()
         ss = line.split()
@@ -86,7 +90,7 @@ while stepno <= stepmax:
     #print(f"# {onbodies=}")
 
     r = numpy.sqrt( numpy.sum( numpy.square(pts[:,0:3]), axis=1 ) )
-    ptss.append( pts[:,0:5] )
+    ptss.append( pts[:,0:6] )
     ixx = pts[:, 3]
 
     rr.append( r )
@@ -108,13 +112,27 @@ if True:
     lum = mass ** 2.5
     mag = -0.921 * numpy.log(lum)
 
-if sdbout is not None:
-    sdbw = sdbio.SDBWriter( sdbout )
+if sdbstem is not None:
+    sdbstem = sdbstem.replace('.sdb','')
+    sdborbits = f"{sdbstem}.orbits.sdb"
+    sdbw = sdbio.SDBWriter( sdborbits )
     timerange = numpy.arange( ptss.shape[0] )
     
     for seqno in orbs: ## range( ptss.shape[1] ):
-        sdbw.writepcles( ptss[:,seqno,0:3], num=seqno, radius=mass[seqno], mag=mag[seqno], opacity=timerange )
+        etot = ptss[:,seqno,5]
+        sdbw.writepcles( ptss[:,seqno,0:3], num=seqno, radius=mass[seqno], mag=mag[seqno], opacity=timerange, dxyz=etot[:,None] )
     sdbw.close()
+
+    speckf = open(f"{sdbstem}.speck", "w")
+    for itime, pts in enumerate(ptss):
+        sdbout = f"{sdbstem}.{itime:04d}.sdb"
+        sdbw = sdbio.SDBWriter( sdbout )
+        etot = pts[:, 5]
+        sdbw.writepcles( pts[:, 0:3], num=numpy.arange(0, len(pts)), radius=mass, mag=mag, opacity=itime, dxyz=etot[:,None] )
+        sdbw.close()
+
+        print(f"sdb -t {itime} {sdbout}", file=speckf)
+
 
 rrmins = []
 rrmaxs = []
@@ -128,22 +146,23 @@ if outstem is None:
         imaxs = 1 + numpy.argwhere( (rbodyi[:-2] < rbodyi[1:-1]) & (rbodyi[1:-1] > rbodyi[2:]) )[:,0]
 
         if len(imins) >= 2 and len(imaxs) >= 2:
+            etotmax = ptss[:, i, 5].max()  # highest total-energy value for this particle's lifetime
             typrmin = rbodyi[imins].mean()
             typrmax = rbodyi[imaxs].mean()
             a = 0.5*(typrmax + typrmin)
             e = 0.5*(typrmax - typrmin) / a
-            rae[i] = dict(isim=ixx[i], a=a, e=e, rmin=rbodyi.min(), rmax=rbodyi.max(), n=len(imins))
+            rae[i] = dict(isim=ixx[i], a=a, e=e, rmin=rbodyi.min(), rmax=rbodyi.max(), n=len(imins), etotmax=etotmax)
 
-    print("# seq  isim     e             a         rmin     rmax   n")
+    print("# seq  isim     e             a         rmin     rmax        n etotmax")
     for i in sorted( rae.keys(), key=lambda i: rae[i]['e'], reverse=True ):
         rd = rae[i]
-        print("%5d %5d %10.6f   %10g   %7.4f  %7.4f  %d" % (i, rd['isim'], rd['e'], rd['a'], rd['rmin'], rd['rmax'], rd['n']))
+        print("%5d %5d %10.6f   %10g   %7.4f  %7.4f  %5d  %7.4f" % (i, rd['isim'], rd['e'], rd['a'], rd['rmin'], rd['rmax'], rd['n'], rd['etotmax']))
 
 if outstem is not None:
     if outstem.endswith('/'):
         outstem += os.path.basename( os.path.dirname(outstem) )
 
-    pointattrnames = [ "seqno", "special", "rmin", "rmax", "a", "ecc", "mass", "bigness" ]
+    pointattrnames = [ "seqno", "special", "rmin", "rmax", "a", "ecc", "mass", "bigness", "etotmax" ]
     pointattrs = numpy.empty( (onbodies, len(pointattrnames)) )
     pointattrs[:,0] = numpy.arange(onbodies)  # attr0: point index in simulation
     pointattrs[:,1] = 0         # attr1: 0=ordinary point (not a special orbit)
@@ -159,9 +178,10 @@ if outstem is not None:
     pointattrs[:,5] = e
     pointattrs[:,6] = mass                  # "mass"
     pointattrs[:,7] = (mass ** 0.5) * 0.005 # "bigness".   0.005 is a magic number, convenient for spacing of stars in "wool" cluster.
+    pointattrs[:,8] = ptss[:,:,5].max(axis=0) # "etotmax"
 
     for stepno in range(nsteps):
-        outfname = outstem + ".%04d.bgeo" % stepno
+        outfname = outstem + ".%04d.bgeo" % (stepno+outbase)
 
         _ = bgeo.BGeoPolyWriter( outfname, points=ptss[stepno,:,0:3], pointattrnames=pointattrnames, pointattrs=pointattrs )
 
@@ -174,7 +194,7 @@ if outstem is not None:
     polyattrs = pointattrs[orbs]
 
     pointattrnames = ["orbframe"]
-    pointattrs = numpy.concatenate( [ numpy.arange(nsteps) for i in orbs ] ).reshape(-1,1)
+    pointattrs = numpy.concatenate( [ outbase+numpy.arange(nsteps) for i in orbs ] ).reshape(-1,1)
 
     ## print(f"poly0: [{len(polys[0])}] dxs {[polys[0][1:,0]-polys[0][:-1,0]]}")
     # print("Writing %d curves, %d vertices to %s" % (len(polys), sum([len(pv) for pv in polys]), outfname))
