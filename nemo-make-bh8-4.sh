eefd853d4db25c33e977a4b3d311acb7dcc5f8d8
Stuart Levy <stuartnlevy@gmail.com>
2025-08-05 10:31:28 -0500

    Recipes for with/without black hole at 6x-slower-than-7-22 speed.   Need dt to be a whole multiple of the internal timestep lest we get skippy.

--- nemo-make-bh8-4.sh
+++ nemo-make-bh8-4.sh
@@ -1,16 +1,15 @@
-rm -f nemotoy/bh8-4_15k_seed.snp nemotoy/bh8-4_15k_m.snp nemotoy/bh8-4_15k_3x.snp
+rm -f nemotoy/bh8-4_15k_seed.snp nemotoy/bh8-4_15k_m.snp nemotoy/bh8-4_15k_6x.snp 
 
-snapcenter times=0 one=t in=nemotoy/gold8-4_15k_3x.snp out=nemotoy/bh8-4_justbh.snp ;# cumbersome way to create 1 particle with m=1 at origin.
+snapcenter times=0 one=t in=nemotoy/gold8-4_15k_6x.snp out=nemotoy/bh8-4_justbh.snp ;# cumbersome way to create 1 particle with m=1 at origin.
 
-snaptrim times=0 in=nemotoy/gold8-4_15k_3x.snp out=- | snapmass in=- massname='n(m)' massexpr='pow(m,p)' masspars=p,-1.5  norm=0.0001 out=nemotoy/bh8-4_15k_m.snp
+snaptrim times=0 in=nemotoy/gold8-4_15k_6x.snp out=- | snapmass in=- massname='n(m)' massexpr='pow(m,p)' masspars=p,-1.5  norm=0.0001 out=nemotoy/bh8-4_15k_m.snp
 snapadd in=nemotoy/bh8-4_15k_m.snp,nemotoy/bh8-4_justbh.snp zerocm=t out=- | addgravity in=- out=- | snapvirial mscale=f rscale=f vscale=t virial=1.0 in=- out=nemotoy/bh8-4_15k_seed.snp 
 
 ls -l nemotoy/bh8-4_15k_seed.snp
 
-# This starts at time 50, so we want to calculate out to 50+125 = 175
 # Use tighter choice of kmax and eps where there's a black hole - close approaches are much more important here than for a cluster with a less huge range of masses
-/usr/bin/time gyrfalcON in=nemotoy/bh8-4_15k_seed.snp step=0.020833333 tstop=42 out=- logfile=nemotoy/bh8-4.gyr kmax=9 eps=0.001 give=mxva | \
+/usr/bin/time gyrfalcON in=nemotoy/bh8-4_15k_seed.snp step=0.01171875 tstop=25 out=- logfile=nemotoy/bh8-4_6x.gyr kmax=9 eps=0.001 give=mxva | \
     addprop in=- out=- add=key value=i | \
-    addgravity in=- out=nemotoy/bh8-4_15k_3x.snp
+    addgravity in=- out=nemotoy/bh8-4_15k_6x.snp
 
-ls -l nemotoy/bh8-4_15k_3x.snp
+ls -l nemotoy/bh8-4_15k_6x.snp
--- nemo-make-nobh8-4.sh
+++ nemo-make-nobh8-4.sh
@@ -1,7 +1,7 @@
-rm -f nemotoy/gold.snp nemotoy/gold8-4_15k_3x.snp
+rm -f nemotoy/gold15k.snp nemotoy/gold8-4_15k_6x.snp
 mkplummer seed=1745450495 out=nemotoy/gold15k.snp nbody=15000 mfrac=0.995 massname='n(m)' massexpr='pow(m,p)' masspars=p,-1.5 massrange=.1,1
-/usr/bin/time gyrfalcON in=nemotoy/gold15k.snp step=0.0208333333 tstop=42 out=- logfile=nemotoy/glog3x kmax=6 eps=0.1 give=mxva | \
+/usr/bin/time gyrfalcON in=nemotoy/gold15k.snp step=0.01171875 tstop=25 out=- logfile=nemotoy/g8-4b.gyr kmax=8 eps=0.02 give=mxva | \
     addprop in=- out=- add=key value=i | \
-    addgravity in=- out=nemotoy/gold8-4_15k_3x.snp
+    addgravity in=- out=nemotoy/gold8-4_15k_6x.snp
 
-ls -l nemotoy/gold8-4_15k_3x.snp
+ls -l nemotoy/gold8-4_15k_6x.snp
